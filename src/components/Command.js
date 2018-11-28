a2e707323548a221c5b82916cd9623bc184b255a
Daniel Marchena Parreira <daniel@Daniels-MacBook-Pro.local>
2018-11-28 16:03:31 -0500

    Add support for BCI commands and EEG data

--- src/components/Command.js
+++ src/components/Command.js
@@ -3,8 +3,8 @@ import { withApollo, Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 
 const COMMANDS_MUTATION = gql`
-  mutation updateCommandsMutation($froms: [String]!, $tos: [String]!, $listenerCommand: String!) {
-    updateCommands(froms: $froms, tos: $tos, listenerCommand: $listenerCommand) {
+  mutation updateCommandsMutation($froms: [String]!, $tos: [String]!, $types: [String]!, $valuesFrom: [String]!, $valuesTo: [String]!, $listenerCommand: String!) {
+    updateCommands(froms: $froms, tos: $tos, types: $types, valuesFrom: $valuesFrom, valuesTo: $valuesTo, listenerCommand: $listenerCommand) {
   		userId
   		from
   		to
@@ -26,6 +26,9 @@ const COMMANDS_QUERY = gql`
       userId
       from
       to
+      valueTo
+      valueFrom
+      type
     }
   }
 `
@@ -34,20 +37,26 @@ class Command extends Component {
   state = {
     froms: [],
     tos: [],
+    types: [],
+    valuesFrom: [],
+    valuesTo: [],
     listenerCommand: "",
     loading: false,
     error: null
   }
 
   addCommand() {
-    const {froms, tos} = this.state;
+    const {froms, tos, types, valuesFrom, valuesTo } = this.state;
     froms.push('')
     tos.push('')
-    this.setState({ froms, tos });
+    types.push('voiceCommand')
+    valuesFrom.push('')
+    valuesTo.push('')
+    this.setState({ froms, tos, types, valuesFrom, valuesTo });
   }
 
   fetchCommands() {
-    const { froms, tos } = this.state
+    const { froms, tos, types, valuesFrom, valuesTo } = this.state
 
     this.setState({loading: true});
 
@@ -59,16 +68,23 @@ class Command extends Component {
         query: USER_QUERY
       }),
     ]).then(data => {
+
       const dataCommands = data[0].data.commands;
       const dataUser = data[1].data.me;
       dataCommands.forEach(command => {
         froms.push(command.from);
         tos.push(command.to);
+        types.push(command.type);
+        valuesFrom.push(command.valueFrom);
+        valuesTo.push(command.valueTo);
       });
       this.setState({
         loading: false,
         froms,
         tos,
+        types,
+        valuesFrom,
+        valuesTo,
         listenerCommand: dataUser.listenerCommand ? dataUser.listenerCommand : ""
       });
     }).catch(() => {
@@ -80,8 +96,79 @@ class Command extends Component {
     this.fetchCommands();
   }
 
+  renderCommandSection({
+    i,
+    from,
+    froms,
+    type,
+    valueFrom,
+    valuesFrom,
+    valueTo,
+    valuesTo
+  }) {
+    if (type === "voiceCommand") {
+      return (
+        <div>
+        Command: <input
+          className="mb2"
+          value={from}
+          onChange={e => {
+            froms[i] = e.target.value
+            this.setState({ froms })
+          }}
+          type="text"
+          placeholder="Command"
+        />
+        </div>
+      );
+    } else if (type === "bciCommand") {
+      return (
+        <div>
+          Command:
+          <select value={from} onChange={e => {
+            froms[i] = e.target.value
+            this.setState({ froms })
+          }}>
+            <option value="attention">Attention</option>
+            <option value="meditation">Meditation</option>
+            <option value="blinking">Blinking</option>
+          </select>
+          From: <input
+            className="mb2"
+            min="1"
+            max="100"
+            style={{width: "60px"}}
+            value={valueFrom}
+            onChange={e => {
+              valuesFrom[i] = e.target.value
+              this.setState({ valuesFrom })
+            }}
+            type="number"
+            placeholder="From"
+          />
+          To: <input
+            className="mb2"
+            min="1"
+            max="100"
+            style={{width: "60px"}}
+            value={valueTo}
+            onChange={e => {
+              valuesTo[i] = e.target.value
+              this.setState({ valuesTo })
+            }}
+            type="number"
+            placeholder="To"
+          />
+        </div>
+      );
+    } else {
+      return <span/>;
+    }
+
+  }
+
   render() {
-    const { froms, tos, listenerCommand, loading, error } = this.state
+    const { froms, tos, types, valuesFrom, valuesTo, listenerCommand, loading, error } = this.state
 
     if (loading) return <div>Fetching</div>
     if (error) return <div>Error</div>
@@ -97,19 +184,41 @@ class Command extends Component {
             <hr />
             {froms.map((from, i) => {
             const to = tos[i];
+            const type = types[i];
+            const valueFrom = valuesFrom[i];
+            const valueTo = valuesTo[i];
 
             return (<span key={i}><div className="flex flex-column mt3">
-              Listener command: <input
-                className="mb2"
-                value={from}
-                onChange={e => {
-                  froms[i] = e.target.value
-                  this.setState({ froms })
-                }}
-                type="text"
-                placeholder="Listener command"
-              />
-              Philips Hue command: <input
+              Command type:
+              <select value={type} onChange={e => {
+                types[i] = e.target.value
+                if (types[i] === "voiceCommand") {
+                  froms[i] = "";
+                } else if (types[i] === "bciCommand") {
+                  froms[i] = "attention";
+                  valuesFrom[i] = "";
+                  valuesTo[i] = "";
+                }
+                this.setState({ types, froms, valuesFrom, valuesTo })
+              }}>
+                <option value="voiceCommand">Voice command</option>
+                <option value="bciCommand">BCI command</option>
+              </select>
+
+              {this.renderCommandSection({
+                i,
+                from,
+                froms,
+                type,
+                valueFrom,
+                valuesFrom,
+                valueTo,
+                valuesTo
+              })}
+
+              <a target="_blank" href="https://github.com/sqmk/huejay#clientgroupsgetall---get-all-groups">
+                Philips HUE command:
+              </a><input
                 className="mb2"
                 value={to}
                 onChange={e => {
@@ -117,12 +226,16 @@ class Command extends Component {
                   this.setState({ tos })
                 }}
                 type="text"
-                placeholder="Philips Hue command"
+                placeholder="Philips HUE command"
               />
+              <div>Example: <pre style={{display:"inline"}}>light.on = true && light.brightness = 200</pre></div>
               <button onClick={e => {
                 tos.splice(i, 1);
                 froms.splice(i, 1);
-                this.setState({ tos, froms })
+                types.splice(i, 1);
+                valuesFrom.splice(i, 1);
+                valuesTo.splice(i, 1);
+                this.setState({ tos, froms, types, valuesFrom, valuesTo })
               }}>Remove</button>
             </div>
             <hr/>
@@ -133,7 +246,7 @@ class Command extends Component {
 
             <Mutation
               mutation={COMMANDS_MUTATION}
-              variables={{ froms, tos, listenerCommand }}
+              variables={{ froms, tos, types, valuesFrom, valuesTo, listenerCommand }}
               onCompleted={() => this.props.history.push('/commands')}
             >
               {mutation => <button onClick={() => {
